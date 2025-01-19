cf741ded3ffa1f633400b173a4db424dfa496539
Yusuke Muto <yusuke.muto@nssys.co.jp>
2025-01-20 02:20:57 +0900

    fifth commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
+<script setup lang="ts"></script>
+
 <template>
-  <v-app>
+  <v-app id="prototype">
+    <v-app-bar>
+      <v-app-bar-title>
+        Vue & Vuetify によるフロントエンド・プロトタイピング
+      </v-app-bar-title>
+      <v-spacer />
+    </v-app-bar>
+
     <v-main>
-      <HelloWorld />
+      <v-container>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="4">
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
-<script setup lang="ts">
-  //
-</script>
+<style lang="css" scoped></style>
